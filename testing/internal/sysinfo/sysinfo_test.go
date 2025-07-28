a56159f9c5099f87678b452cd36edc189d6cd7ef
not-matthias <matthias@codspeed.io>
2025-07-28 19:24:05 +0200

    chore: fork testing

---
+++ testing/internal/sysinfo/sysinfo_test.go
@@ -0,0 +1,16 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sysinfo_test
+
+import (
+	"testing"
+
+	. "github.com/CodSpeedHQ/codspeed-go/testing/internal/sysinfo"
+)
+
+func TestCPUName(t *testing.T) {
+	t.Logf("CPUName: %s", CPUName())
+	t.Logf("osCPUInfoName: %s", XosCPUInfoName())
+}
