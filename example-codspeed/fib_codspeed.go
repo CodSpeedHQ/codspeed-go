889fd730ab2ca2c06e45e97fec0f4b928427a2de
not-matthias <matthias@codspeed.io>
2025-08-27 18:03:37 +0200

    chore: update benchmarks

--- example-codspeed/fib_codspeed.go
+++ example-codspeed/fib_codspeed.go
@@ -5,31 +5,31 @@ import (
 )
 
 func BenchmarkFibonacci10(b *testing.B) {
-	// b.Run("fibonacci(40)", func(b *testing.B) {
-	// 	for i := 0; i < b.N; i++ {
-	// 		fibonacci(10)
-	// 	}
-	// })
-	// b.Run("fibonacci(20)", func(b *testing.B) {
-	// 	for i := 0; i < b.N; i++ {
-	// 		fibonacci(20)
-	// 	}
-	// })
-	b.RunParallel(func(b *testing.PB) {
-		for b.Next() {
-			fibonacci(30)
+	b.Run("fibonacci(10)", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			fibonacci(10)
 		}
 	})
+	b.Run("fibonacci(20)", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			fibonacci(20)
+		}
+	})
+	// b.RunParallel(func(b *testing.PB) {
+	// 	for b.Next() {
+	// 		fibonacci(30)
+	// 	}
+	// })
 }
 
 func BenchmarkFibonacci20(b *testing.B) {
 	for b.Loop() {
-		fibonacci(30)
+		fibonacci(20)
 	}
 }
 
-// func BenchmarkFibonacci30(b *testing.B) {
-// 	for i := 0; i < b.N; i++ {
-// 		fibonacci(30)
-// 	}
-// }
+func BenchmarkFibonacci30(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		fibonacci(30)
+	}
+}
