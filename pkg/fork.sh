f974240283859d954b216063716fa92dab319cc0
not-matthias <matthias@codspeed.io>
2025-09-09 17:23:23 +0200

    feat: fork quicktest

--- pkg/fork.sh
+++ pkg/fork.sh
@@ -21,3 +21,9 @@ pushd slogassert
 replace_in_go_files '"github.com/thejerf/slogassert' '"github.com/CodSpeedHQ/codspeed-go/pkg/slogassert'
 fix_project
 popd
+
+git clone -b v1.14.6 https://github.com/frankban/quicktest
+pushd quicktest
+replace_in_go_files '"github.com/frankban/quicktest' '"github.com/CodSpeedHQ/codspeed-go/pkg/quicktest'
+fix_project
+popd
