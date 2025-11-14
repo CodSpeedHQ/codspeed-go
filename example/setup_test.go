259b1991d38efcf5680abfce9023268fc21530cc
not-matthias <matthias@codspeed.io>
2025-11-14 11:17:46 +0100

    fix: incorrect first iteration time

--- example/setup_test.go
+++ example/setup_test.go
@@ -18,12 +18,35 @@ func BenchmarkLargeSetup(b *testing.B) {
 
 func BenchmarkLargeSetupInLoop(b *testing.B) {
 	var result uint64
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		expensiveOperation()
-		b.StartTimer()
 
+	// The `testing` package starts the timer automatically at the beginning of the benchmark. If we try to
+	// stop the timer before the expensiveOperation, and then start it again afterwards we'll run into an issue
+	// where the difference from the function start to the first b.StopTimer() call is included in the timing.
+	// ```
+	// 	for i := 0; i < b.N; i++ {
+	// 		b.StopTimer()
+	// 		expensiveOperation()
+	// 		b.StartTimer()
+	// 		b.Log("Running iteration", i, b.Elapsed())
+	//
+	// 		result = actualWork()
+	// 		b.Log("Running iteration", i, b.Elapsed())
+	// 	}
+	// ```
+	// This will print two timings:
+	// setup_test.go:26: Running iteration 0 782ns
+	// setup_test.go:29: Running iteration 0 7.560911ms
+	//
+	// However, the first timing (782ns) is misleading because it's not related to the benchmark. To avoid this, we have to
+	// stop the timer in the beginning of the benchmark function.
+	b.StopTimer()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		expensiveOperation()
+
+		b.StartTimer()
 		result = actualWork()
+		b.StopTimer()
 	}
 	runtime.KeepAlive(result)
 }
