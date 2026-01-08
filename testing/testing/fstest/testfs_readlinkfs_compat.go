58937524f7c1a614f2f710f5a76b5c7fa3838530
not-matthias <matthias@codspeed.io>
2026-01-08 16:17:01 +0100

    chore: remove testing

--- testing/testing/fstest/testfs_readlinkfs_compat.go
+++
@@ -1,9 +0,0 @@
-//go:build !go1.25
-// +build !go1.25
-
-package fstest
-
-import "io/fs"
-
-// No-op on Go versions before 1.25 since fs.ReadLinkFS doesn't exist.
-func (t *fsTester) checkReadLinkFS(path string, einfo fs.FileInfo, fentry string) {}
