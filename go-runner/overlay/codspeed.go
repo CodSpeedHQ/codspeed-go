bc5be1bb9a83f40457df9db22f4ffb2337d00984
not-matthias <matthias@codspeed.io>
2026-01-16 16:03:16 +0100

    fix(go-runner): limit maximum `b.Loop()` execution time

--- go-runner/overlay/codspeed.go
+++ go-runner/overlay/codspeed.go
@@ -26,6 +26,42 @@ type codspeed struct {
 	// Indicates whether a measurement has been saved already. This aims to prevent saving measurements
 	// twice, because `b.Loop()` saves them internally as well but is also called from runN
 	savedMeasurement bool
+
+	// The start time of the first b.Loop() call. This includes the benchmark execution
+	// time, including the overhead of start/stop the timer each loop iteration.
+	loopStartTime time.Time
+}
+
+const BenchMaxTimeMult = 3
+
+// Modified version of the `stopOrScaleLoop` function to also take into account the
+// overhead of start/stop the timer each loop iteration.
+//
+// If we have large setups/teardowns within the loop, they won't count as benchmark time
+// which could cause the benchmark to run for too long.
+func (b *B) stopOrScaleBLoopCodspeed() bool {
+	// The total duration must be at most N times the requested benchtime
+	actualT := time.Since(b.loopStartTime)
+	if actualT >= b.benchTime.d*BenchMaxTimeMult {
+		return false
+	}
+
+	t := b.Elapsed()
+	if t >= b.benchTime.d {
+		// We've reached the target
+		return false
+	}
+
+	// Loop scaling
+	goalns := b.benchTime.d.Nanoseconds()
+	prevIters := int64(b.loop.n)
+	b.loop.n = uint64(predictN(goalns, prevIters, actualT.Nanoseconds(), prevIters))
+	if b.loop.n&loopPoisonMask != 0 {
+		// The iteration count should never get this high, but if it did we'd be
+		// in big trouble.
+		panic("loop iteration target overflow")
+	}
+	return true
 }
 
 func findGitRoot() (string, error) {
